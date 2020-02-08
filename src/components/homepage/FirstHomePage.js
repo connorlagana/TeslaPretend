94e81c708d79fcd27eec1089d270d71adfc3e96f
Connor Lagana <connor.lagana@gmail.com>
2020-02-08 14:47:08 -0500

    tesla proj

---
+++ src/components/homepage/FirstHomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import mainHeroNarrow from "../../images/mainHeroNarrow.jpg";
+
+const firstHomePage = () => {
+  return (
+    <div>
+      <img src={mainHeroNarrow} id="mainHero"/>
+    </div>
+  );
+};
+
+export default firstHomePage;
