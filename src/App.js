8f95a0c8e7ba14a0920a9c4f1c7fbb2805fb16d8
Connor Lagana <connor.lagana@gmail.com>
2020-02-10 17:51:09 -0500

    broken in my heart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.css";
+
 import FirstHomePage from "./components/homepage/FirstHomePage.js";
-import NavBar from "./components/nav/NavBar.js";
+import BuyModel3 from "./components/buy/BuyModel3.js";
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +14,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FirstHomePage />
+        <Route exact path="/" component={FirstHomePage} />
+        <Route exact path="/OrderModel3" component={BuyModel3} />
       </div>
     );
   }
