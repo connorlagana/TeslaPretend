15f756821f526883d674f317f610793d13cb7b55
Connor Lagana <connor.lagana@gmail.com>
2020-02-11 16:25:47 -0500

    register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Login from "./components/login/Login.js";
 
 import "./App.css";
 
 import FirstHomePage from "./components/homepage/FirstHomePage.js";
 import Model3Battery from "./components/buy/model3/Model3Battery.js";
+import Login from "./components/login/Login.js";
+import Register from "./components/login/Register";
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class App extends Component {
         <Route exact path="/" component={FirstHomePage} />
         <Route exact path="/model3/design/battery" component={Model3Battery} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </div>
     );
   }
---
+++ src/components/login/Register.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import registerPic from "../../images/registerPic.jpg";
+import registerPic2 from "../../images/registerPic2.jpg";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      backPic: registerPic
+    };
+  }
+
+  changeBackPic = () => {
+    if (this.state.backPic === registerPic) {
+      this.setState({
+        backPic: registerPic2
+      });
+    } else {
+      this.setState({
+        backPic: registerPic
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <img
+          src={this.state.backPic}
+          onClick={this.changeBackPic}
+          id="backLoginImage"
+        />
+        <div className="actualRegister">
+          <div className="backActReg">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
