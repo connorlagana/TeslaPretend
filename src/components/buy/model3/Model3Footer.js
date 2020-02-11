cf42c843f4340e9713bab5ffa2952abc7817d7e0
Connor Lagana <connor.lagana@gmail.com>
2020-02-11 09:21:50 -0500

    right model3 buy siv

--- src/components/buy/model3/Model3Footer.js
+++ src/components/buy/model3/Model3Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
 
 const Model3Footer = () => {
   return (
     <div className="model3Footer">
-      <h1>Model3Footer goes here</h1>
+      <div className="cashDiv">
+        <h3>Cash</h3>
+        <KeyboardArrowDown />
+      </div>
+      <div id="pricesDiv">
+        <div>
+          <h2>$33,690</h2>
+          <p>After potential savings</p>
+        </div>
+        <div>
+          <h2>$39,990</h2>
+          <p>Purchase price</p>
+        </div>
+      </div>
     </div>
   );
 };
