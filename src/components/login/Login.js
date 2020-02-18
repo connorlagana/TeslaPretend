686bb22d1a4c541712eb3ab88835e54af866901d
Connor Lagana <connor.lagana@gmail.com>
2020-02-18 10:26:49 -0500

    thank you for coming to my birthday party

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import loginPic from "../../images/loginPic.jpg";
 import loginPic2 from "../../images/loginPic2.jpg";
 
+import {
+  registerUser,
+  loginUser,
+  verifyUser
+} from "../../services/api_helper.js";
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -9,6 +15,7 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
+      currentUser: null,
       backPic: loginPic
     };
   }
@@ -25,6 +32,21 @@ class Login extends Component {
     }
   };
 
+  handleChange = e => {
+    const { name, value } = e.target;
+    // Same as const name = e.target.name // const value = e.target.value
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleLogin = async (e, loginData) => {
+    e.preventDefault();
+    const currentUser = await loginUser(loginData);
+    this.setState({ currentUser });
+    this.props.history.push("/garages");
+  };
+
   render() {
     return (
       <div className="login">
@@ -37,8 +59,16 @@ class Login extends Component {
           <div className="backActLog">
             <div id="signInDiv">Sign In</div>
             <form>
-              <input type="email" id="emailInputLogin" placeholder="Email" />
               <input
+                onChange={this.handleChange}
+                name="email"
+                type="email"
+                id="emailInputLogin"
+                placeholder="Email"
+              />
+              <input
+                onChange={this.handleChange}
+                name="password"
                 type="password"
                 id="emailInputLogin"
                 placeholder="Password"
@@ -48,7 +78,9 @@ class Login extends Component {
                   I forgot my password
                 </div>
               </div>
-              <button id="singInButton">Sign In</button>
+              <button id="singInButton" onClick={this.handleLogin}>
+                Sign In
+              </button>
             </form>
             <div>
               <a id="createAcct" href="/register">
